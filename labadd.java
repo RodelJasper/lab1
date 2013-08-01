c1fe2219146a8ebd47dfff4deaff1e7964c7e7b4
RodelJasper <rroj173@aucklanduni.ac.nz>
2013-08-02 10:56:42 +1200

    adder

--- labadd.java
+++ labadd.java
@@ -1,16 +1,12 @@
 
 
-import java.util.Scanner;
- 
-class AddNumbers
-{
+
    public static void main(String args[])
    {
       int x, y, z;
       System.out.println("Enter two integers to calculate their sum ");
-      Scanner in = new Scanner(System.in);
-      x = in.nextInt();
-      y = in.nextInt();
+      x = 4;
+      y = 7;
       z = x + y;
       System.out.println("Sum of entered integers = "+z);
    }
